87e090c3f2741fd3f338033b2e7efb3cf5634709
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-23 17:56:41 +0200

    passage du numéro d'étudiant dans l'URL

--- enonce.js
+++ enonce.js
@@ -1,5 +1,15 @@
-window.énoncé = (i) => `
-## hello
-world
-- step ${i}
+const render = (variant, studentId) => `
+# Évaluation individuelle
+
+## Exercice 1
+
+Rendre votre définition du mot ${variant(["exercice", "évaluation", "examen"])}.
+
+## Exercice 2
+
+Consulter l'énoncé d'un autre étudiant, exemple: [/${studentId + 1}](/${studentId + 1}).
 `;
+
+const variantPicker = (studentId) => (variants) => variants[studentId % variants.length]
+
+window.énoncé = (studentId) => render(variantPicker(studentId), studentId);
