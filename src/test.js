f9952834525f8321d63145fb8f2891854927720f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-31 16:06:06 +0100

    ❌ variantPicker() should support negative studentId

--- src/test.js
+++ src/test.js
@@ -18,6 +18,7 @@ test('variantPicker returns the right variant based on studentId', t => {
   t.is(variantPicker(1234)(['even', 'odd']), 'even');
   t.is(variantPicker(1235)(['even', 'odd']), 'odd');
   t.is(variantPicker(1236)(['even', 'odd']), 'even');
+  t.is(variantPicker(-1)(['even', 'odd']), 'odd');
 });
 
 test('hashCode can turn an email address into a number', t => {
