524f30413a693412968df105e4ef00ff7fb22ecd
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-10-31 14:08:16 +0100

    add tests => move modulo() to index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Modulo that also works for big integers stored as string.
+ * It only works for positive numbers.
+ * cf https://stackoverflow.com/a/16019504/592254
+ */
+function modulo (divident, divisor) {
+  const partLength = 10;
+  while (divident.length > partLength) {
+      const part = divident.substring(0, partLength);
+      divident = (part % divisor) +  divident.substring(partLength);          
+  }
+  return divident % divisor;
+}
+
+/**
+ * Hashes a string into a number.
+ * cf https://stackoverflow.com/a/7616484/592254
+ */
+function hashCode (str) {
+  var hash = 0, i, chr;
+  for (i = 0; i < str.length; i++) {
+    chr   = str.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+try {
+  // make the functions also loadable from Node.js
+  module.exports = {
+    modulo,
+    hashCode,
+  };
+} catch (err) {}
