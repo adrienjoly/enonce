e854a6d0202013e9f812de56c417bd99b2599f88
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2021-05-23 23:19:26 +0200

    refacto 2

--- src/cli-helpers.js
+++ src/cli-helpers.js
@@ -13,10 +13,14 @@ function prependVariables (template, variables) {
     assert(typeof variantProps === "object" && !Array.isArray(variantProps));
     return Object.entries(variantProps).map(([propKey, propVal]) => renderVariantProp(propKey, propVal)).join('\n')
   };
+  const renderVariantVariable = (variableKey, variableVal) => {
+    assert(typeof variableVal === "object" && !Array.isArray(variableVal));
+    return `  ${variableKey}: {\n${renderVariantProps(variableVal)}\n  },`;
+  };
   return [
    '<!-- prepended variables: ${ Object.assign(this, {',
       ...Object.entries(variables).map(
-        ([variableKey, variableVal]) => `  ${variableKey}: {\n${renderVariantProps(variableVal)}\n  },` // serialize the javascript object, assuming that variableVal is an object in which each prop is an array of variant values
+        ([variableKey, variableVal]) => renderVariantVariable(variableKey, variableVal) // serialize the javascript object, assuming that variableVal is an object in which each prop is an array of variant values
       ),
    '}) } -->',
    template,
